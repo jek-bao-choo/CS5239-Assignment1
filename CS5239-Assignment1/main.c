e8486fd529a29b226f7ab6996da686aa9c8d662a
Jek Bao CHOO <jerchoo@outlook.com>
2016-09-30 06:42:22 +0800

    initial commit

---
+++ CS5239-Assignment1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CS5239-Assignment1
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Initial commit with this\n");
+    return 0;
+}
