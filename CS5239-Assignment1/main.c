a5a44e797d92617e91b11af1e2cb68c0e3cf8280
Jek Bao CHOO <jek.bao.choo@gmail.com>
2016-09-30 21:06:56 +0800

    another solution

--- CS5239-Assignment1/main.c
+++ CS5239-Assignment1/main.c
@@ -1,131 +1,46 @@
-//
-//  main.c
-//  CS5239-Assignment1
-//
-//
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void allocate_mem(double*** arr, int rows, int cols);
-void deallocate_mem(double*** arr, int n);
-void printMatrix(double** a, int rows, int cols);
-void printMatrixE(double** a, int rows, int cols);
-void multMatrixp(double **A, double **B, double **C,int r1,int c1,int r2,int c2);
-void init(double*** a, int rows,int cols);
+#include "mm.h"
 
 int main(int argc, char* argv[])
 {
+    int i;
     
-    int ro1, co1, ro2, co2;
-    double **a, **b, **c;
+    if(argc != 2)
+    {
+        printf("Usage: %s <size_of_square_matrix>\n", argv[0]);
+        exit(1);
+    }
     
-    
-    
-    //ro1=10;
-    //co1=10000;
-    //ro2=10000;
-    //co2=33;
-    
-    ro1=atoi(argv[1]);
-    co1=atoi(argv[2]);
-    ro2=atoi(argv[3]);
-    co2=atoi(argv[4]);
-    
-    
-    init(&a,ro1,co1);
-    init(&b,ro2,co2);
-    allocate_mem(&c,ro1,co2);
+    SIZE = atoi(argv[1]);
+    //main function invoking init
+    init(&A, &B); // Pass references to A and B
     
     clock_t begin, end;
     double time_spent;
+    
     begin = clock();
-    multMatrixp(a, b, c, ro1, co1, ro2, co2);
+    
+    mm(A, B, C);
+    
     end = clock();
+    
     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    
     printf("Elapsed time: %.2lf seconds.\n", time_spent);
     
-    printMatrix(a,ro1,co1);
-    printMatrix(b,ro2,co2);
-    printMatrixE(c,ro1,co2);
+    for(i = 0; i < SIZE; i++)
+        free((void *)A[i]);
+    free((void *)A);
     
+    for(i = 0; i < SIZE; i++)
+        free((void *)B[i]);
+    free((void *)B);
     
-    deallocate_mem(&a,ro1);
-    deallocate_mem(&b,ro2);
-    deallocate_mem(&c,ro1);
+    for(i = 0; i < SIZE; i++)
+        free((void *)C[i]);
+    free((void *)C);
     
     return 0;
-}
-
-//______________________________________________________________________________
-void allocate_mem(double*** arr, int rows, int cols)
-{
-    int i;
-    *arr = (double**)malloc(rows*sizeof(double*));
-    for( i=0; i<rows; i++)
-        (*arr)[i] = (double*)malloc(cols*sizeof(double));
-}
-
-//______________________________________________________________________________
-void deallocate_mem(double*** arr, int rows){
-    int i;
-    for (i = 0; i < rows; i++)
-        free((*arr)[i]);
-    free(*arr);
-}
-
-//______________________________________________________________________________
-
-void init(double*** a, int rows,int cols)
-{
-    int i, j;
-    
-    *a= (double**) malloc(rows*sizeof(double*));
-    for(i=0;i<rows;i++)
-        (*a)[i]=(double*)malloc(cols*sizeof(double));
-    for(i=0;i<rows;i++)
-        for(j=0;j<cols;j++)
-            (*a)[i][j] = rand()%1000;
-}
-
-
-//______________________________________________________________________________
-void printMatrix(double** a, int rows, int cols)
-{
-    int i, j;
-    printf("Matrix[%d][%d]\n",rows,cols);
-    for(i=0;i<rows;i++){
-        for(j=0;j<cols;j++)
-            printf("%7.1lf ",a[i][j]);
-        printf("\n");
-    }
-    printf("\n");
-}
-
-//______________________________________________________________________________
-void printMatrixE(double** a, int rows, int cols)
-{
-    int i, j;
-    printf("Matrix[%d][%d]\n",rows,cols);
-    for(i=0;i<rows;i++){
-        for(j=0;j<cols;j++)
-            printf("%9.2e ",a[i][j]);
-        printf("\n");
-    }
-    printf("\n");
-}
-
-
-//______________________________________________________________________________
-
-void multMatrixp(double **A, double **B, double **C,int ro1,int co1,int ro2,int co2)
-{
-    int i, j, k;
-    for(i = 0; i < ro1; i++) {
-        for(j = 0; j < co2; j++) {
-            C[i][j] = 0;
-            for(k = 0; k < co1; k++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
 }
---
+++ CS5239-Assignment1/mm.h
@@ -0,0 +1,60 @@
+//
+//  mm.h
+//  CS5239-Assignment1
+
+#ifndef MM_H
+#define MM_H
+
+#include <stdlib.h>
+#include <time.h>
+
+int SIZE, NTHREADS;
+int **A, **B, **C;
+
+void init(int ***Aptr, int ***Bptr)
+{
+    int    **A; // Define a local pointer to keep rest of the code intact
+    int    **B; // Define a local pointer to keep rest of the code intact
+    
+    int i, j;
+    
+    A = (int**)malloc(SIZE * sizeof(int *));
+    for(i = 0; i < SIZE; i++)
+        A[i] = malloc(SIZE * sizeof(int));
+    
+    B = (int**)malloc(SIZE * sizeof(int *));
+    for(i = 0; i < SIZE; i++)
+        B[i] = malloc(SIZE * sizeof(int));
+    
+    C = (int**)malloc(SIZE * sizeof(int *));
+    for(i = 0; i < SIZE; i++)
+        C[i] = malloc(SIZE * sizeof(int));
+    
+    srand(time(NULL));
+    
+    for(i = 0; i < SIZE; i++) {
+        for(j = 0; j < SIZE; j++) {
+            A[i][j] = rand()%100;
+            B[i][j] = rand()%100;
+        }
+    }
+    // End of function
+    *Aptr = A;
+    *Bptr = B;
+}
+
+void mm(int **A, int **B, int **C)
+{
+    int i, j, k;
+    
+    for(i = 0; i < SIZE; i++) {
+        for(j = 0; j < SIZE; j++) {
+            C[i][j] = 0;
+            for(k = 0; k < SIZE; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+#endif
